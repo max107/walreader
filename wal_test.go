d30442b3f23f3cd111116076616499fd8bc5a04e
Maksim Falaleev <maxfalaleev1@gmail.com>
2024-08-19 10:50:59 +0800

    disable parallel

--- wal_test.go
+++ wal_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/max107/walreader"
 )
 
-func TestWal(t *testing.T) {
+func TestAllTables(t *testing.T) {
 	ctx := context.TODO()
 
 	config, err := pgx.ParseConfig(os.Getenv("DATABASE_URL"))
@@ -49,73 +49,105 @@ func TestWal(t *testing.T) {
 		`insert into words (word) values ('foo'), ('bar');`,
 	}
 
-	t.Run("tables", func(t *testing.T) {
-		listener := walreader.NewListener(
-			conn.PgConn(),
-			conn.TypeMap(),
-			"numbers_slot",
-			"public",
-			[]string{"numbers"},
-		)
+	listener := walreader.NewListener(
+		conn.PgConn(),
+		conn.TypeMap(),
+		"words_slot",
+		"public",
+		nil,
+	)
 
-		require.NoError(t, listener.Clean(ctx))
-		require.NoError(t, listener.Init(ctx))
+	require.NoError(t, listener.Clean(ctx))
+	require.NoError(t, listener.Init(ctx))
 
-		for _, sql := range inserts {
-			_, err := conn.Exec(ctx, sql)
-			require.NoError(t, err)
-		}
+	for _, sql := range inserts {
+		_, err := conn.Exec(ctx, sql)
+		require.NoError(t, err)
+	}
 
-		var wg sync.WaitGroup
-		wg.Add(3)
+	var wg sync.WaitGroup
+	wg.Add(5)
 
-		go func() {
-			if err := listener.Start(ctx, func(events []*walreader.Event) error {
-				for range events {
-					wg.Done()
-				}
-
-				return nil
-			}); err != nil {
-				t.Failed()
+	go func() {
+		if err := listener.Start(ctx, func(events []*walreader.Event) error {
+			for range events {
+				wg.Done()
 			}
-		}()
 
-		wg.Wait()
+			return nil
+		}); err != nil {
+			t.Failed()
+		}
+	}()
+
+	wg.Wait()
+}
+
+func TestSpecificTables(t *testing.T) {
+	ctx := context.TODO()
+
+	config, err := pgx.ParseConfig(os.Getenv("DATABASE_URL"))
+	require.NoError(t, err)
+
+	conn, err := pgx.ConnectConfig(ctx, config)
+	require.NoError(t, err)
+
+	googleuuid.Register(conn.TypeMap())
+
+	t.Cleanup(func() {
+		_ = conn.Close(ctx)
 	})
 
-	t.Run("all_tables", func(t *testing.T) {
-		listener := walreader.NewListener(
-			conn.PgConn(),
-			conn.TypeMap(),
-			"words_slot",
-			"public",
-			nil,
-		)
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, os.Kill)
+	defer stop()
 
-		require.NoError(t, listener.Clean(ctx))
-		require.NoError(t, listener.Init(ctx))
+	sqls := []string{
+		`drop table if exists numbers;`,
+		`drop table if exists words;`,
+		`create table numbers (number int);`,
+		`create table words (word varchar(255));`,
+	}
 
-		for _, sql := range inserts {
-			_, err := conn.Exec(ctx, sql)
-			require.NoError(t, err)
-		}
+	for _, sql := range sqls {
+		_, err := conn.Exec(ctx, sql)
+		require.NoError(t, err)
+	}
 
-		var wg sync.WaitGroup
-		wg.Add(5)
+	inserts := []string{
+		`insert into numbers (number) values (1), (2), (3);`,
+		`insert into words (word) values ('foo'), ('bar');`,
+	}
 
-		go func() {
-			if err := listener.Start(ctx, func(events []*walreader.Event) error {
-				for range events {
-					wg.Done()
-				}
+	listener := walreader.NewListener(
+		conn.PgConn(),
+		conn.TypeMap(),
+		"numbers_slot",
+		"public",
+		[]string{"numbers"},
+	)
 
-				return nil
-			}); err != nil {
-				t.Failed()
+	require.NoError(t, listener.Clean(ctx))
+	require.NoError(t, listener.Init(ctx))
+
+	for _, sql := range inserts {
+		_, err := conn.Exec(ctx, sql)
+		require.NoError(t, err)
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	go func() {
+		if err := listener.Start(ctx, func(events []*walreader.Event) error {
+			for range events {
+				wg.Done()
 			}
-		}()
 
-		wg.Wait()
-	})
+			return nil
+		}); err != nil {
+			t.Failed()
+		}
+	}()
+
+	wg.Wait()
 }
