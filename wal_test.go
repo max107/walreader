63d6b964d7b1b7829e56bc47568be34f14ecf29e
Maksim Falaleev <maxfalaleev1@gmail.com>
2025-07-05 07:25:30 +0800

    improve tests

--- wal_test.go
+++ wal_test.go
@@ -2,6 +2,7 @@ package walreader_test
 
 import (
 	"context"
+	"fmt"
 	"github.com/jackc/pgx/v5"
 	"github.com/max107/walreader"
 	"github.com/stretchr/testify/require"
@@ -24,6 +25,8 @@ func TestWalReader(t *testing.T) {
 		conn, err := pgx.ConnectConfig(t.Context(), config)
 		require.NoError(t, err)
 
+		prepare(t, conn, "all_slot")
+
 		sqls := []string{
 			`drop table if exists words;`,
 			`drop table if exists numbers;`,
@@ -31,7 +34,6 @@ func TestWalReader(t *testing.T) {
 			`create table words (word varchar(255), number int);`,
 			`alter table numbers replica identity full;`,
 			`alter table words replica identity full;`,
-			`select pg_drop_replication_slot('all_slot');`,
 			`select pg_create_logical_replication_slot('all_slot', 'pgoutput');`,
 			`drop publication if exists all_slot;`,
 			`create publication all_slot for table words;`,
@@ -80,6 +82,8 @@ func TestWalReader(t *testing.T) {
 		conn, err := pgx.ConnectConfig(t.Context(), config)
 		require.NoError(t, err)
 
+		prepare(t, conn, "words_slot")
+
 		sqls := []string{
 			`drop table if exists words;`,
 			`drop table if exists numbers;`,
@@ -87,7 +91,6 @@ func TestWalReader(t *testing.T) {
 			`create table words (word varchar(255), number int);`,
 			`alter table numbers replica identity full;`,
 			`alter table words replica identity full;`,
-			`select pg_drop_replication_slot('words_slot');`,
 			`select pg_create_logical_replication_slot('words_slot', 'pgoutput');`,
 			`drop publication if exists words_slot;`,
 			`create publication words_slot for table words;`,
@@ -136,12 +139,12 @@ func TestWalReader(t *testing.T) {
 		conn, err := pgx.ConnectConfig(t.Context(), config)
 		require.NoError(t, err)
 
+		prepare(t, conn, "specific_fields")
+
 		sqls := []string{
 			`drop table if exists words;`,
 			`create table words (word varchar(255), number int);`,
 			`alter table words replica identity full;`,
-			`select pg_terminate_backend(active_pid) from pg_replication_slots where slot_name = 'specific_fields';`,
-			`select pg_drop_replication_slot('specific_fields');`,
 			`select pg_create_logical_replication_slot('specific_fields', 'pgoutput');`,
 			`drop publication if exists specific_fields;`,
 			`create publication specific_fields for table words (word);`,
@@ -153,11 +156,11 @@ func TestWalReader(t *testing.T) {
 			require.NoError(t, err)
 		}
 
-		testWal(t, conn, "specific_fields", 3)
+		walcheck(t, conn, "specific_fields", 3)
 	})
 }
 
-func testWal(t *testing.T, conn *pgx.Conn, name string, expect int64) {
+func walcheck(t *testing.T, conn *pgx.Conn, name string, expect int64) {
 	t.Helper()
 
 	listener := walreader.NewListener(conn, name)
@@ -190,4 +193,14 @@ func testWal(t *testing.T, conn *pgx.Conn, name string, expect int64) {
 	require.True(t, found)
 	cancel()
 	wg.Wait()
+}
+
+func prepare(t *testing.T, conn *pgx.Conn, name string) {
+	t.Helper()
+	for _, sql := range []string{
+		fmt.Sprintf(`select pg_terminate_backend(active_pid) from pg_replication_slots where slot_name = '%s';`, name),
+		fmt.Sprintf(`select pg_drop_replication_slot('%s');`, name),
+	} {
+		_, _ = conn.Exec(t.Context(), sql)
+	}
 }
